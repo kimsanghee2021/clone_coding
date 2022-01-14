26057cddaf6e45d1a0230892e65e4d130615987c
kimsanghee2021 <tkdgmltjddnr@naver.com>
2022-01-14 23:43:40 +0900

    메인과 메뉴 작업 중

--- 공인협회사/asset/js/main.js
+++ 공인협회사/asset/js/main.js
@@ -1,10 +1,11 @@
 $(function(){
-    var menuBtn = $('.toggle_menu');
-    var menu_list = $('.aside_menu_wrap .aside_menu_list');
-    var menu_bg = $('.aside_menu_bg');
+    var menuBtn = $('.toggle_menu'),
+    menu_list = $('.aside_menu_wrap .aside_menu_list'),
+    menu_bg = $('.aside_menu_bg');
     
     menuBtn.click(function(){
         menu_list.addClass('show');
         menu_bg.fadeIn();
+        $('#wrapper').css({position:'fixed'});
     });
 });
