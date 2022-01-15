3715c6b69bb896cd20ad691676d6594d787176e0
kimsanghee2021 <tkdgmltjddnr@naver.com>
2022-01-15 12:10:00 +0900

    토글메뉴 작업 중 60%

--- 공인협회사/asset/js/main.js
+++ 공인협회사/asset/js/main.js
@@ -1,11 +1,20 @@
 $(function(){
     var menuBtn = $('.toggle_menu'),
     menu_list = $('.aside_menu_wrap .aside_menu_list'),
-    menu_bg = $('.aside_menu_bg');
-    
+    menu_bg = $('.aside_menu_bg'),
+    menu_close = $('.aside_menu_close');
+
+    //토글메뉴 노출 - open
     menuBtn.click(function(){
         menu_list.addClass('show');
         menu_bg.fadeIn();
         $('#wrapper').css({position:'fixed'});
+    });
+
+    //토글메뉴 닫기 - close
+    menu_close.click(function(){
+        menu_list.removeClass('show');
+        menu_bg.fadeOut();
+        $('#wrapper').css({position:'relative'});
     });
 });
