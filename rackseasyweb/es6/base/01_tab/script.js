6ac7a74337210ee9b0ae7c8a4493d1a9557026da
kimsanghee2021 <tkdgmltjddnr@naver.com>
2022-01-22 14:21:04 +0900

    javascript로 탭 메뉴 만들어보기

--- rackseasyweb/es6/base/01_tab/script.js
+++ rackseasyweb/es6/base/01_tab/script.js
@@ -2,3 +2,10 @@
 탭메뉴를 클릭하면 관련 내용이 나타나고
 하이라이트 배경이 활성화된 메뉴위치로 이동합니다.
 */
+let tabMenu =  document.querySelectorAll('.tab-menu li');
+let tabContent = document.querySelectorAll('.tab-content > div');
+console.log(tabContent);
+
+tabMenu.addEventListener('click',function(){
+    this.classList.add('');
+});
