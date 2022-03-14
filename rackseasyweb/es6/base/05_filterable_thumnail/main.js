c9cb44dfb2d28404fb9b2ec8850ab799a5c0e1fb
kimsanghee <tkdgmltjddnr@naver.com>
2022-03-14 23:13:13 +0900

    filter 갤러리

--- rackseasyweb/es6/base/05_filterable_thumnail/main.js
+++ rackseasyweb/es6/base/05_filterable_thumnail/main.js
@@ -1,6 +1,6 @@
 const imgList = document.querySelector('.image-list');
 const btns = document.querySelectorAll('.view-options button');
-const imgListItem = imgList.querySelectorAll('li');
+const imgListItems = imgList.querySelectorAll('li');
 const active = 'active';
 const listView = 'list-view';
 const gridView = 'grid-view';
@@ -11,17 +11,6 @@ const dNone = 'd-none';
 
 
 //버튼을 클릭하면 그 버튼에게만 active가 들어오게 해라.
-
-// btns.forEach(function(btn,idx){
-//     btn.addEventListener('click',function(){
-//         for(const el of btns){
-//             el.parentElement.classList.remove(active);
-//             btns[idx].parentElement.classList.add(active);
-//             console.log(btns[idx]);
-//         }
-//     });
-// });
-
 for(const btn of btns){
     btn.addEventListener('click',function(){
         const parent = this.parentElement;
@@ -39,3 +28,60 @@ for(const btn of btns){
         }
     });
 }
+//input range를 변할때마다 갤러리 넓이가 변하게 짜기
+const rangeInput = document.querySelector('input[type="range"]');
+rangeInput.addEventListener('input',function(){
+    document.documentElement.style.setProperty('--minRangeValue',`${this.value}px`);
+    //선택자.style.css속성명 = 값
+    //선택자.style.backgroundColor = 'blue';
+    //선택자.style.setProperty('backgroudnColor','blue');
+});
+
+//검색키워드 필터적용하기
+const captions = imgList.querySelectorAll('figcaption p:first-child');
+const myArray = [];
+let counter = 1;
+
+for(const caption of captions){
+    myArray.push({
+        id : counter++,
+        text : caption.textContent
+    });
+}
+
+
+const searchInput = document.querySelector('input[type="search"]');
+const phptpsCounter = document.querySelector('.toolbar .counter span');
+
+searchInput.addEventListener('keyup',keupHandler);
+
+function keupHandler(){
+    for(const item of imgListItems){
+        item.classList.add(dNone);
+    }
+    const keywords = this.value;
+    const filteredArray = myArray.filter(el=>{
+        return el.text.includes(keywords);
+    });
+    console.log(filteredArray);
+    if(filteredArray.length > 0){
+        for(const el of filteredArray){
+            document.querySelector(`.image-list li:nth-child(${el.id})`).classList.remove(dNone);
+        }
+    }
+    phptpsCounter.textContent = filteredArray.length;
+}
+
+//filter
+/*
+var arr1 = [1,4,567,8,5,10,35];
+var arr2 = arr1.filter(function(el){
+    return el % 5 == 0;
+});
+console.log(arr2);
+
+var arr1 = [1,4,567,8,5,10,35];
+var arr2 = arr1.filter(el=> el % 5 == 0);
+console.log(arr2);
+
+*/
